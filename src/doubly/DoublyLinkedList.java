fcd521190436688ac8c06750bbe9ed7d565e1c01
deeksha kashyap <deekshakas>
2019-03-01 09:33:42 +0530

    init commit

---
+++ src/doubly/DoublyLinkedList.java
@@ -0,0 +1,63 @@
+package doubly;
+
+class Node{
+    int data;
+    Node next;
+    Node previous;
+}
+
+public class DoublyLinkedList{
+    Node head;
+    int size;
+
+    public static void main(String[] args) {
+        DoublyLinkedList obj=new DoublyLinkedList();
+        for (int i = 0; i < 5; i++) {
+            Node node=new Node();
+            node.data=(i+1)*1000;
+            obj.insert(node);
+        }
+        obj.display();
+
+    }
+
+    void display() {
+        System.out.println("Doubly linkedlist");
+        Node temp = head;
+        System.out.print("null<----->");
+        while (temp != null) {
+            System.out.print(temp.data + "<--->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    void delete(){
+        if(head==null){
+            System.out.println("empty list");
+        }else{
+            Node temp=head;
+            while(temp.next!=null){
+                temp=temp.next ;
+            }
+            System.out.println("deleting node"+ temp.data+ "from last");
+            temp=temp.previous;
+            temp.next=null;
+        }
+    }
+
+    //insert
+    void insert(Node node){
+        System.out.println("inserting node" + node.data + "at last");
+        if(head==null){
+            head=node;
+        }else{
+            Node temp=head;
+            while(temp.next!=null){
+                temp=temp.next;
+            }
+            node.previous=temp;
+            temp.next=node;
+        }
+    }
+}
